5894b8261a66d8c68b3858a43108eef60941b6db
MichelleFn <annafn48@gmail.com>
2021-01-05 14:20:00 +0200

    write and read from file

--- src/main/java/DataStructures/HuffmanTree.java
+++ src/main/java/DataStructures/HuffmanTree.java
@@ -6,7 +6,6 @@
 package DataStructures;
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -14,10 +13,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 /**
  *
@@ -28,16 +24,16 @@ public class HuffmanTree {
     public static PriorityQueue<Nodes> tree = new PriorityQueue<Nodes>(128, new MyComparator());
 
     public static ArrayDeque<Nodes> ad = new ArrayDeque<>();
-    public static ArrayDeque<Integer> ad2 = new ArrayDeque<>();
+    public static ArrayDeque<String> ad2 = new ArrayDeque<>();
 
-    //public static ArrayList<Nodes> tree = new ArrayList<Nodes>(128, new MyComparator());
-//    public PriorityQueue<Nodes> getTree() {
-//        return tree;
-//    }
-//
-//    public void setTree(PriorityQueue<Nodes> tree) {
-//        this.tree = tree;
-//    }
+    public PriorityQueue<Nodes> getTree() {
+        return tree;
+    }
+
+    public void setTree(PriorityQueue<Nodes> tree) {
+        this.tree = tree;
+    }
+
     public static File FileToNode(File file) throws FileNotFoundException, IOException {
 
         int AsciiValue;
@@ -67,14 +63,13 @@ public class HuffmanTree {
         File treeFile = new File("tree.dat");
         Nodes root = null;
 
-
         while (tree.size() > 1) {
 
             Nodes firstNode = tree.remove();
             Nodes secondNode = tree.remove();
             Nodes nNode = new Nodes();
             nNode.frequency = firstNode.getFrequency() + secondNode.getFrequency();
-            nNode.character='-';
+            nNode.character = '-';
             nNode.child2 = firstNode;
             nNode.child1 = secondNode;
             //tree.add(nNode);
@@ -82,103 +77,20 @@ public class HuffmanTree {
             root = nNode;
             tree.add(root);
         }
-//       Iterator value = tree.iterator();
-//
-//                // Displaying the values after iterating through the queue 
-//                System.out.println("The iterator values are: ");
-//                while (value.hasNext()) {
-//                    System.out.println(value.next().toString());
-//                }
-        //System.out.println(tree.peek().toString());
-//        String result = findPath('a',root,"");
-//        System.out.println(result);
-        //System.out.println(root.toString());
 
-      
-//        FileOutputStream fos = new FileOutputStream(treeFile);
-//        ObjectOutputStream oos = new ObjectOutputStream(fos);
-//
-//        oos.writeObject(tree);
-//
-//        oos.close();
-        //ad.push(root);
-        //ad2.push(0);
-        preorderIter(root, "");
-        System.out.println("Huffman Tree Created");
+        FileOutputStream fos = new FileOutputStream(treeFile);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        oos.writeObject(tree);
+
+        oos.close();
+       
+       
+        //System.out.println("Huffman Tree Created");
         return treeFile;
 
     }
 
-    public static void preorderIter(Nodes root, String s) {
-
-        if (root == null) {
-            return;
-        }
-
-        //while (ad.isEmpty()) {
-        if (root.child1 == null && root.child2 == null && Character.isLetterOrDigit(root.character)) {
-//            System.out.println(root.character + ":" + s);
-//            Iterator value = ad2.iterator();
-//
-//            // Displaying the values after iterating through the queue 
-//            System.out.println("The iterator values are: ");
-//            while (value.hasNext()) {
-//                System.out.println(value.next().toString());
-//            }
-            System.out.println(root.character + ":" + s);
-            return;
-        }
-
-        //ad2.clear();
-        preorderIter(root.child2, s + "0");
-        //ad2.add(0);
-        preorderIter(root.child1, s + "1");
-        //ad2.add(1);
-
-    }
+   
 
 }
-
-//public static void preorderIter(Nodes root) {
-//
-//        if (root == null) {
-//            return;
-//        }
-//
-//        ArrayDeque<Nodes> ad = new ArrayDeque<>();
-//        ArrayDeque<Integer> ad2 = new ArrayDeque<>();
-//        ad.push(root);
-//        ad2.push(0);
-//
-//        while (!ad.isEmpty()) {
-//
-//            Nodes n = ad.pop();
-//
-//            if (n.child1 != null) {
-////                ad.push(n.child1);
-//                ad2.push(0);
-//                preorderIter(n.child1);
-//            }
-//            if (n.child2 != null) {
-////               ad.push(n.child2);
-//                ad2.push(1);
-//                preorderIter(n.child2);
-//            }
-//            if ((n.child1 == null) && (n.child2 == null)) {
-//                Iterator value = ad2.iterator();
-//
-//                // Displaying the values after iterating through the queue 
-//                System.out.println("The iterator values are: ");
-//                while (value.hasNext()) {
-//                    System.out.println("code "+value.next()+" character"+n.character);
-//                }
-////                System.out.println(ad2.iterator().toString());
-////                
-////                for (int elem : ad2) {
-//////                    System.out.println(ad2.descendingIterator());
-////                   
-////                    System.out.println("code =" + elem  + "  character =" + n.character);
-//////                    System.out.println("fsakhgjadsAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-////                }
-//                ad2.clear();
-
--- src/main/java/DataStructures/PrintTreeCode.java
+++ src/main/java/DataStructures/PrintTreeCode.java
@@ -5,13 +5,17 @@
  */
 package DataStructures;
 
+import static DataStructures.HuffmanTree.ad2;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
@@ -21,23 +25,52 @@ import java.util.PriorityQueue;
  */
 public class PrintTreeCode {
 
-    public static PriorityQueue<Nodes> pq = new PriorityQueue<Nodes>(128, new MyComparator());
+    public static PriorityQueue<Nodes> tree = new PriorityQueue<Nodes>(128, new MyComparator());
+
+    public static File codeFile = new File("code.dat");
 
     public static File treeCode(File HuffmanFile) throws FileNotFoundException, IOException, ClassNotFoundException {
         File codeFile = new File("code.dat");
         ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(HuffmanFile));
 
         Object obj = inputStream.readObject();
-        //System.out.println(obj);
-        try {
+        tree = (PriorityQueue) obj;
+        Nodes root = tree.peek();
+        System.out.println(tree);
 
-            System.out.println(obj);
+        ArrayDeque<String> ad2 = new ArrayDeque<>();
+        preorderIter(root, ad2);
+        return codeFile;
 
-        } catch (Exception e) {
-            System.out.println("\n" + "End of file reached");
+    }
+
+    public static void preorderIter(Nodes root, ArrayDeque s) throws FileNotFoundException, IOException {
+
+        if (root.child1 == null && root.child2 == null) {
+
+            DataOutputStream output = new DataOutputStream(new FileOutputStream(codeFile));
+            System.out.println("The iterator values " + root.character + " are: " + "\n");
+           // System.out.println("The iterator values " + root.character + " are: " + s + "\n");
+
+            for (int i = 0; i < ad2.size(); i++) {
+                System.out.print(ad2.toArray()[i]);
+
+            }
+
+            return;
         }
 
-        return codeFile;
+        if (root.child2 != null) {
+            ad2.add("0");
+            preorderIter(root.child2, ad2);
+        }
+        if (root.child1 != null) {
+
+            ad2.add("1");
+            preorderIter(root.child1, ad2);
+        }
+        ad2.clear();
+
     }
 
 }
